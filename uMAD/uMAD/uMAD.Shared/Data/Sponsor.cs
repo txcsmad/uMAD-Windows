6bfdfa90e90725bd18b0f1922128f7925dc54e78
MelanieRivera <obits13@outlook.com>
2015-01-25 21:40:08 -0600

    Sponsor

---
+++ uMAD/uMAD/uMAD.Shared/Data/Sponsor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace uMAD.Data
+{
+    public class Sponsor
+    {
+        public BitmapImage CompanyLogo { get; set; }
+        public string CompanyURL { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
