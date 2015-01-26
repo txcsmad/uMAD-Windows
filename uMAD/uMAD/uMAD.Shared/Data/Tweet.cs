8bea46a0c122f89d2f8481b431d266018387bb68
MelanieRivera <obits13@outlook.com>
2015-01-25 20:35:18 -0600

    TwitterFeed UC. Sponsors UC

---
+++ uMAD/uMAD/uMAD.Shared/Data/Tweet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace uMAD.Data
+{
+    public class Tweet
+    {
+        public BitmapImage twitterImage { get; set; }
+        public string username { get; set; }
+        public string displayName { get; set; }
+        public string tweetText { get; set; }
+
+    }
+}
---
+++ uMAD/uMAD/uMAD.WindowsPhone/TwitterFeedUserControl.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using uMAD.Data;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace uMAD
+{
+    public sealed partial class TwitterFeedUserControl : UserControl
+    {
+        private ObservableCollection<Tweet> tweetData;
+        private bool isLoaded;
+
+        public TwitterFeedUserControl()
+        {
+            this.InitializeComponent();
+            tweetData = new ObservableCollection<Tweet>();
+
+            Loaded += TwitterFeedUserControl_Loaded;
+        }
+
+        private void TwitterFeedUserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            if(!isLoaded)
+            {
+                //TODO add Twitter feeds
+                isLoaded = true;
+            }
+            
+        }
+
+
+    }
+}
