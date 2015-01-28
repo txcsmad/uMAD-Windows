82887ecfa49ff1f4e360f7cafd9edf6481d7b859
janabimustafa <mustafa@mustafataleb.com>
2015-01-27 22:48:52 -0600

    parse

--- uMAD/uMAD/uMAD.Shared/Data/ScheduleSession.cs
+++ uMAD/uMAD/uMAD.Shared/Data/ScheduleSession.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Xml.Serialization;
 namespace uMAD.Data
 {
-    public class ScheduleSession
+    [Parse.ParseClassName("Events")]
+    public class ScheduleSession : Parse.ParseObject
     {
+        [Parse.ParseFieldName("speaker")]
         public string SpeakerName { get; set; }
+
+        [Parse.ParseFieldName("company")]
         public string SubjectName { get; set; }
+
+        [Parse.ParseFieldName("startTime")]
         public DateTime Time { get; set; }
     }
 }
