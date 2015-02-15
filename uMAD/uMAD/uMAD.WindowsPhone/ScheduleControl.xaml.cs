edcfd8aed236a6d65d11b8b78f2cc69636f18f97
janabimustafa <mustafa@mustafataleb.com>
2015-02-14 18:03:26 -0600

    Added SemanticZoom to ScheduleControl

--- uMAD/uMAD/uMAD.WindowsPhone/ScheduleControl.xaml.cs
+++ uMAD/uMAD/uMAD.WindowsPhone/ScheduleControl.xaml.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
+using Parse;
 using uMAD.Data;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -22,14 +23,16 @@ namespace uMAD
 {
     public sealed partial class ScheduleControl : UserControl
     {
-        private ObservableCollection<ScheduleSession> scheduleData;
+        public ScheduleViewModel viewModel;
+        //private ObservableCollection<ScheduleSession> scheduleData;
         private bool isLoaded;
         public event EventHandler LoadingTweets;
         public event EventHandler LoadedTweets;
         public ScheduleControl()
         {
             this.InitializeComponent();
-            scheduleData = new ObservableCollection<ScheduleSession>();
+            viewModel = new ScheduleViewModel();
+            //scheduleData = new ObservableCollection<ScheduleSession>();
             Loaded += ScheduleControl_Loaded;
         }
 
@@ -37,32 +40,52 @@ namespace uMAD
         {
             if (!isLoaded)
             {
+                semanticControl.ViewChangeStarted += SemanticControl_ViewChangeStarted;
+                (semanticControl.ZoomedOutView as ListViewBase).ItemsSource = viewModel.Items;
+
                 if (LoadingTweets != null)
                     LoadingTweets(this, new EventArgs());
-                dataList.ItemsSource = scheduleData;
+                this.DataContext = viewModel;
+                scheduleCollection.Source = viewModel.Items;
                 await RefreshData();
                 isLoaded = true;
                 if (LoadedTweets != null)
                     LoadedTweets(this, new EventArgs());
             }
 
-            if (scheduleData.Count == 0)
+            if (viewModel.Count == 0)
                 emptyControl.Visibility = Visibility.Visible;
+        }
+
+        private void SemanticControl_ViewChangeStarted(object sender, SemanticZoomViewChangedEventArgs e)
+        {
+            if (e.SourceItem == null || e.SourceItem.Item == null)
+                return;
+
+            if (e.SourceItem.Item.GetType() == typeof(GroupList<ScheduleSession>))
+            {
+                var group = (GroupList<ScheduleSession>)e.SourceItem.Item;
+
+                //var group = viewModel.Items.FirstOrDefault(d => ((char)d.Key) == hi.Name);
+                if (group != null)
+                    e.DestinationItem = new SemanticZoomLocation() { Item = group };
+            }
         }
 
         private async Task RefreshData()
         {
-            scheduleData.Clear();
+            viewModel.Clear();
             var data = await LoadData();
-            foreach (var item in data)
-                scheduleData.Add(item);
+            viewModel.Populate(data);
         }
 
         private async Task<IEnumerable<ScheduleSession>> LoadData()
         {
-            var query = new Parse.ParseQuery<ScheduleSession>();
-            query = query.WhereGreaterThanOrEqualTo("startTime", DateTime.Now);
-            query = query.OrderBy("startTime");
+            var query = from item in new ParseQuery<ScheduleSession>()
+                        orderby item.Time ascending
+                        select item;
+            //query = query.WhereGreaterThanOrEqualTo("startTime", DateTime.Now);
+            //query = query.OrderBy("startTime");
             return await query.FindAsync();
         }
         private async Task<List<ScheduleSession>> LoadFakeData()
@@ -87,5 +110,48 @@ namespace uMAD
                 ((Frame)Window.Current.Content).Navigate(typeof(SessionView));
             }
         }
+    }
+
+    public class ScheduleViewModel
+    {
+        public ObservableCollection<GroupList<ScheduleSession>> Items { get; set; }
+        private string[] titles = { "past sessions", "upcoming sessions" };
+        public int Count { get; private set; }
+
+        public ScheduleViewModel()
+        {
+            Items = new ObservableCollection<GroupList<ScheduleSession>>();
+        }
+
+        public void Populate(IEnumerable<ScheduleSession> list)
+        {
+            Count = Count + list.Count();
+            var pastGroup = new GroupList<ScheduleSession>() { Title = titles[0] };
+            var upcomingGroup = new GroupList<ScheduleSession>() { Title = titles[1] };
+
+            var pastList = from item in list
+                           where item.Time < DateTime.Now
+                           select item;
+            var upcomingList = from item in list
+                               where item.Time >= DateTime.Now
+                               select item;
+            pastGroup.AddRange(pastList);
+            upcomingGroup.AddRange(upcomingList);
+            if (upcomingGroup.Count > 0)
+                Items.Add(upcomingGroup);
+            if (pastGroup.Count > 0)
+                Items.Add(pastGroup);
+        }
+
+        public void Clear()
+        {
+            Items.Clear();
+        }
+    }
+
+    public class GroupList<T> : List<T>
+    {
+        public string Title { get; set; }
+
     }
 }
